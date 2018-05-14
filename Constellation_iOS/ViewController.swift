52badc72234585f8defb5be5acb43a397f8213ef
llHoYall <hoya128@gmail.com>
2018-05-14 14:48:14 +0900

    Merge from dev branch

--- Constellation_iOS/ViewController.swift
+++ Constellation_iOS/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +18,8 @@ class ViewController: UIViewController {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
+	
+	@IBAction func backButtonAction(_ sender: Any) {
+		self.navigationController?.popViewController(animated: true)
+	}
 }
-
